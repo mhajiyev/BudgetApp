0cfd92aae99080954a58868e9dd308d4a77af8d2
Mustafa Hajiyev <mustafahajiyev@Mustafas-MacBook-Pro.local>
2018-05-26 20:53:53 -0400

    deleting items from budgetCtrller

--- app.js
+++ app.js
@@ -54,6 +54,17 @@ var budgetController = (function (){
 			return newItem;
 		},
 
+		deleteItem: function(type, id){
+			var ids = data.allItems[type].map(function(current){
+				return current.id;
+			});
+			var index = ids.indexOf(id);
+
+			if (index !== -1){
+				data.allItems[type].splice(index,1);
+			}
+		},
+
 		calculateBudget: function(){
 			//calculate total income and expense
 			calculateTotal('exp');
@@ -210,10 +221,10 @@ var controller = (function (budgetCtrl,UICtrl){
    	if (itemID){
    			splitID = itemID.split('-');
    			type = splitID[0];
-   			ID = splitID[1];
+   			ID = parseInt(splitID[1]);
 
    			//delete the item from data structure
-
+   			budgetCtrl.deleteItem(type,ID);
    			//deleting from UI
 
    			//updating/show new budget
