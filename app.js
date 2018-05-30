fd780987a697233accc0af1f6b3220b1bb4b8969
Mustafa Hajiyev <mhajiyev@uwaterloo.ca>
2018-05-29 23:45:32 -0400

    formating numbers and rounding to 2 decimal places

--- app.js
+++ app.js
@@ -137,6 +137,22 @@ var UIController = (function() {
  		expensesPercLabel: '.item__percentage'
     };
 
+    	var formatNumber = function(num, type){
+			num = Math.abs(num);
+			num = num.toFixed(2);
+
+			numSplit = num.split('.');
+
+			int = numSplit[0];
+
+			if (int.length > 3){
+				int = int.substr(0,int.length - 3) + ',' + int.substr(int.length-3,3);
+			}
+			dec = numSplit[1];
+
+			return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
+
+		};
 	return {
 		getinput: function(){
 			return {
@@ -162,7 +178,7 @@ var UIController = (function() {
 			// Replacing the placeholder text with some actual data
 			newHtml = htmlCode.replace('%id%',obj.ID);
 			newHtml = newHtml.replace('%description%',obj.description);
-			newHtml = newHtml.replace('%value%',obj.value);
+			newHtml = newHtml.replace('%value%',formatNumber(obj.value,type));
 			//Insert the HTML into DOM
 			document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
 		},
@@ -196,10 +212,13 @@ var UIController = (function() {
 			var temp = document.getElementById(selectorID);
 			temp.parentNode.removeChild(temp);
 		},
+
+
 		displayBudget: function(obj){
-			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+			obj.budget > 0 ? type = 'inc' : type = 'exp';
+			document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget,type);
+			document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc,'inc');
+			document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp,'exp');
 			
 
 			if (obj.percentage > 0){
